6ee3413e2459a09f0f566b690632124c2145e936
minhee71 <minhee71@cs104u-2e.acad.fullerton.edu>
2019-09-07 13:31:58 -0700

    Completed the requested assignment.

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,25 @@
+// Name: Peter Choi
 // This program calculates a person's height in feet (') and inches (").
+#include <iostream>
+
+int main ()
+
+  {
+      // Assignment of variables.
+      int height_inches_total;
+      int height_feet;
+      int height_inches;
+
+      // Request the total height from user.
+      std::cout << "What is the persons height in inches: ";
+      std::cin >> height_inches_total;
+
+      // Calculation of the height to feet/inches.
+      height_feet = height_inches_total / 12;
+      height_inches = height_inches_total % 12;
+
+      // Display the height in feet inches.
+      std::cout << "\nThe person is " << height_feet << "\' " <<  height_inches << "\".\n";
+
+  return 0;
+  }
